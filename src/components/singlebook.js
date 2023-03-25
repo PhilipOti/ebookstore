f2f6a6cb5b2424fb5d85afc0883a356837eb3f24
PhilipOti <philipoti2012@gmail.com>
2023-03-25 23:57:12 +0300

    add new file redirects; fix react router bug

--- src/components/singlebook.js
+++ src/components/singlebook.js
@@ -1,22 +1,55 @@
 import Accordion from "react-bootstrap/Accordion";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
-function SingleBook({books,loading}) {
-    if (!loading) {
-      return <h2>Loading</h2>;
+function SingleBook({ books, loading }) {
+  if (!loading) {
+    return <h2>Loading</h2>;
+  }
+
+  const starsRating = (rating) => {
+    const starsRatingList = [];
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        starsRatingList.push(
+          <AiFillStar key={i} className="ratingStar"></AiFillStar>
+        );
+      } else {
+        starsRatingList.push(<AiOutlineStar key={i}></AiOutlineStar>);
+      }
     }
+
+    return starsRatingList;
+  };
+
   return (
     <Accordion flush>
-        {books.map((book)=>{
-            return (
-              <Accordion.Item eventKey={book.id}>
-                <Accordion.Header>{book.title}</Accordion.Header>
-                <Accordion.Body>
-                  {book.description}
-                </Accordion.Body>
-              </Accordion.Item>
-            );
-        })}
-      
+      {books.map((book) => {
+        return (
+          <div key={book.id}>
+            <Accordion.Item eventKey={book.id}>
+              <Accordion.Header>
+                <strong>
+                  <em>{book.title}</em>
+                </strong>
+              </Accordion.Header>
+              <Accordion.Body>
+                {book.description} <hr />
+                <div className="d-md-flex justify-content-between">
+                  <div>
+                    <p>
+                      <b>Author : </b>
+                      {book.author}
+                    </p>
+                  </div>
+                  <div>
+                    <b>Rating:</b> {starsRating(book.rating)}
+                  </div>
+                </div>
+              </Accordion.Body>
+            </Accordion.Item>
+          </div>
+        );
+      })}
     </Accordion>
   );
 }
